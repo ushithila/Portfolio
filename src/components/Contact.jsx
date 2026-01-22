87d04cc4eaa22588d2c96f30df6bd7387cf6daa6
Shithila Urshi <78228551+ushithila@users.noreply.github.com>
2026-01-22 16:17:24 -0500

    almost done with contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
-import { Mail } from "lucide-react"
+import { Linkedin, Mail, MapPin } from "lucide-react"
+import { cn } from "@/lib/util";
 
 export const Contact = () => {
     return <section className="py-24 px-4 relative bg-secondary/30">
@@ -26,13 +27,50 @@ export const Contact = () => {
                                     className="text-muted-foreground hover:text-primary transition-colors"> ushithila@gmail.com</a>
                             </div>
                         </div>
+
+                        <div className="flex items-start space-x-4">
+                            <div className="p-3 rounded-full bg-primary/10">
+                                <MapPin className="h-6 w-6 text-primary" />
+                            </div>
+                            <div>
+                                <h4 className="font-medium"> Location</h4>
+                                <a className="text-muted-foreground hover:text-primary transition-colors"> New York, USA</a>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="pt-8">
-                        <h4> Connect With Me</h4>
+                        <h4 className="font-medium mb-4"> Connect With Me</h4>
                         <div className="flex space-x-4 justify"></div>
+                        <a href="linkedin.com/in/shithila-urshi/" target="_blank">
+                            <Linkedin />
+                        </a>
                     </div>
+
                 </div>
+
+                <div className="bg-card p-8 rounded-lg shadow-xs">
+                    <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+                    <form className="space-y-6">
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium mb-2"> Your Name</label>
+                            <input type="text" id="name" name="name" required className="w-full px-4 py-3 rounded-md border border-input bg-background focus-outline-hidden focus:ring-2 focus:ring-primary" placeholder="Name" />
+                        </div>
+
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium mb-2"> Your Email</label>
+                            <input type="email" id="email" name="email" required className="w-full px-4 py-3 rounded-md border border-input bg-background focus-outline-hidden focus:ring-2 focus:ring-primary" placeholder="Email" />
+                        </div>
+
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium mb-2"> Your Message</label>
+                            <textarea id="message" name="message" required className="w-full px-4 py-3 rounded-md border border-input bg-background focus-outline-hidden focus:ring-2 focus:ring-primary resize-none" placeholder="Message" />
+                        </div>
+
+                        <button type="submit" className={cn("")}></button>
+                    </form>
+                </div>
+
             </div>
         </div>
     </section>
